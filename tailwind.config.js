20774388e9acaecbcd9d0407c3b521e58a2f57ed
RalfiSlask <nilssonnarki@gmail.com>
2024-04-22 13:04:54 +0200

    :lipstick: add custom variables in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundPrimary: '#f8f7f4',
+        backgroundFooter: '#d3d2d2',
+        backgroundThird: '#717171',
+        backgroundFourth: '#f0f0f0',
+        backgroundDot: '#bbb',
+        backgroundButton: '#a43bc4',
+        borderColor: '#ececec',
+        color: '#434746',
+      },
+      boxShadow: {
+        button: '2px 2px 10px rgb(182, 180, 180)',
+      },
+      fontFamily: {
+        fontSegoe: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+        fontHandlee: ['Handlee', 'cursive'],
+        fontDancing: ['Handlee', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
