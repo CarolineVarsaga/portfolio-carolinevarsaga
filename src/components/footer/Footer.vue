4b895771b3f3367afadd968c56f336badadf1396
RalfiSlask <nilssonnarki@gmail.com>
2024-04-22 13:20:17 +0200

    :sparkles: add Footer component

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="h-[35px] flex-center bg-backgroundFooter">
+    Copyright &copy; Caroline Vårsaga 2024
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
