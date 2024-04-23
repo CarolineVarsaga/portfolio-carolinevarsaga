1293071a544d6830125b7fa15e3a06d0866561c5
RalfiSlask <nilssonnarki@gmail.com>
2024-04-23 06:54:15 +0200

    :sparkles: add section heading component in shared

---
+++ src/components/shared/SectionHeading.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2 class="section-heading">{{ text }}</h2>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{ text: string }>();
+</script>
+
+<style scoped></style>
