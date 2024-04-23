664497d0458d0ee34554ba8ff9ee995644640417
RalfiSlask <nilssonnarki@gmail.com>
2024-04-23 06:52:42 +0200

    :sparkles: add Navbar with looped navButtons

---
+++ src/components/header/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="flex items-end gap-[0.5rem] h-full">
+    <NavButton v-for="(button, index) in navButtons" :key="index" :button="button" />
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import NavButton from './NavButton.vue';
+
+interface INavButton {
+  text: string;
+  href: string;
+}
+
+const navButtons = ref<INavButton[]>([
+  { text: 'Om mig', href: '' },
+  { text: 'Portfolio', href: '' },
+  { text: 'Kontakt', href: '' },
+]);
+</script>
+
+<style scoped></style>
