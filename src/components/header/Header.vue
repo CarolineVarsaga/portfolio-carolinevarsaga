b086cc36c2c111a6b49972e2a0c5f625fb01bc35
RalfiSlask <nilssonnarki@gmail.com>
2024-04-23 06:53:15 +0200

    :sparkles: add Header component with current child components

---
+++ src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="flex-between fixed top-0 w-screen px-[1rem] h-[10rem]">
+    <Logo />
+    <Navbar />
+  </header>
+</template>
+
+<script setup lang="ts">
+import Logo from './Logo.vue';
+import Navbar from './Navbar.vue';
+</script>
+
+<style scoped></style>
