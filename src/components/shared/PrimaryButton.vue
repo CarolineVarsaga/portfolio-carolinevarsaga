7d283d3de952b2ac8f36820df169bf282ffd335a
RalfiSlask <nilssonnarki@gmail.com>
2024-04-23 06:53:36 +0200

    :sparkles: add primary button components in shared

---
+++ src/components/shared/PrimaryButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="primary-button">{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{ text: string }>();
+</script>
+
+<style scoped></style>
