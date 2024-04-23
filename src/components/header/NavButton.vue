a90116e551c8987b67467e43b85b19a95de32de2
RalfiSlask <nilssonnarki@gmail.com>
2024-04-23 06:52:03 +0200

    :sparkles: add Navbutton with text and href props

---
+++ src/components/header/NavButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <a class="border-none h-[35px] w-[100px] cursor-pointer" :href="button.href">{{ button.text }}</a>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface INavButton {
+  text: string;
+  href: string;
+}
+
+defineProps<{ button: INavButton }>();
+</script>
+
+<style scoped></style>
