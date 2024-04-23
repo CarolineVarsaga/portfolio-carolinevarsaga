74109ddf808f262b738d388fab896cfb92ba53f8
RalfiSlask <nilssonnarki@gmail.com>
2024-04-23 06:51:43 +0200

    :sparkles: add Logo

---
+++ src/components/header/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full flex flex-col gap-2">
+    <h1 class="font-fontDancing text-[2rem] font-semibold">Caroline Vårsaga</h1>
+    <p class="italic">Front end developer - student</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
