7089c90a6b0a8ccf5756f9c8b18f85fb6e2b4888
RalfiSlask <nilssonnarki@gmail.com>
2024-04-24 18:39:58 +0200

    :sparkles: add LIA component

---
+++ src/components/main/LIA.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <SectionHeading text="LIA - Lärande i arbete" />
+    <p>Under hösten 2024 går jag ut på min första LIA-period. Då kommer jag ha mer än 1 års studier i bagaget.</p>
+    <p>
+      Jag önskar en LIA-plats där jag kan fortsätta utvecklas och få använda min kreativa sida till att bygga upp
+      hemsidor/webbapplikationer. Ser framemot att lära mig mer inom bland annat SCSS och JavaScript.
+    </p>
+    <Skills />
+    <p>LIA-period 1: 14 oktober 2024 - 20 december 2024</p>
+    <p>LIA-period 2: 3 februari 2025 - 23 maj 2025</p>
+    <p>
+      Utbildningens hemsida:
+      <a href="https://medieinstitutet.se/utbildningar/front-end-developer/" target="_blank">Front End Developer</a>
+    </p>
+
+    <PrimaryButton text="Kontakta mig" />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PrimaryButton from '../shared/PrimaryButton.vue';
+import SectionHeading from '../shared/SectionHeading.vue';
+import Skills from './Skills.vue';
+</script>
+
+<style scoped></style>
