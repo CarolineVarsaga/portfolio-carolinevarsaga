8fabf8976a5228834fb6d75e466e668563a9274e
RalfiSlask <nilssonnarki@gmail.com>
2024-04-24 18:40:36 +0200

    :sparkles: add contact component

---
+++ src/components/main/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <SectionHeading text="Kontakta mig" />
+    <p>E-post: carolinevarsaga@gmail.com</p>
+    <p>Tel: 073-0840995</p>
+    <h3>Besök mig</h3>
+    <SocialMedia />
+  </section>
+</template>
+
+<script setup lang="ts">
+import SectionHeading from '../shared/SectionHeading.vue';
+import SocialMedia from './SocialMedia.vue';
+</script>
+
+<style scoped></style>
