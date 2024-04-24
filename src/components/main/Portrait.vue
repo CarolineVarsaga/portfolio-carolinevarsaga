150d7bbb7cfbb07c935c7b8ccea017e9a2ecd64d
RalfiSlask <nilssonnarki@gmail.com>
2024-04-24 18:38:36 +0200

    :sparkles: add portrait component

---
+++ src/components/main/Portrait.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    :src="Portrait"
+    width="300px"
+    height="300px"
+    alt="Porträttbild. Bruna ögon, leende mun. Hår ner till axlarna."
+    loading="lazy"
+    class="img_face"
+  />
+</template>
+
+<script setup lang="ts">
+import Portrait from '../../assets/images/carolinevarsaga.jpg';
+</script>
+
+<style scoped></style>
