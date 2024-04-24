6f714ac7838581c34f5a9bd422fa539f98efd4fc
RalfiSlask <nilssonnarki@gmail.com>
2024-04-24 18:39:25 +0200

    :sparkles: add skills component where i loop over the skill component

---
+++ src/components/main/Skills.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Kompetenser:</p>
+    <ul>
+      <Skill v-for="(skill, index) in skills" :key="index" :skill="skill" />
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const skills = [
+  'HTML5',
+  'Sass',
+  'Typescript',
+  'MongoDB',
+  'MySQL',
+  'React',
+  'Vue',
+  'Agila arbetsmetoder',
+  'Git',
+  'Figma',
+  'Photoshop',
+  'Illustrator',
+];
+import Skill from './Skill.vue';
+</script>
+
+<style scoped></style>
