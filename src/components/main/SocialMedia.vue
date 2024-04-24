cbd54fec0f346e151bcc665fabb644b49c894026
RalfiSlask <nilssonnarki@gmail.com>
2024-04-24 18:40:12 +0200

    :sparkles: add Social Media component

---
+++ src/components/main/SocialMedia.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex gap-6">
+    <a href="http://github.com/carolinevarsaga" target="_blank">
+      <img :src="GithubLogo" width="96" height="96" alt="github" loading="lazy" />
+    </a>
+    <a href="http://linkedin.com/in/caroline-varsaga" target="_blank"
+      ><img :src="LinkedinLogo" width="96" height="96" alt="linkedin" loading="lazy"
+    /></a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GithubLogo from '../../assets/icons/github.svg';
+import LinkedinLogo from '../../assets/icons/linkedin.svg';
+</script>
+
+<style scoped></style>
