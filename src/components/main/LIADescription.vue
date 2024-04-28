e0a8df2e5ee8cb4b012873d27ef5ef6893513478
RalfiSlask <nilssonnarki@gmail.com>
2024-04-28 13:56:13 +0200

    :sparkles: add LIADescription component

---
+++ src/components/main/LIADescription.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <p>Under hösten 2024 går jag ut på min första LIA-period. Då kommer jag ha mer än 1 års studier i bagaget.</p>
+    <p>
+      Jag önskar en LIA-plats där jag kan fortsätta utvecklas och få använda min kreativa sida till att bygga upp
+      hemsidor/webbapplikationer. Ser framemot att lära mig mer inom bland annat SCSS och JavaScript.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
