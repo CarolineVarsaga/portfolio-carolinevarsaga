716404d25cddac745bf16890ec381baf75e50cee
RalfiSlask <nilssonnarki@gmail.com>
2024-04-28 17:52:29 +0200

    :sparkles: import Header, Main and Footer components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Main from './components/main/Main.vue';
+import Header from './components/header/Header.vue';
+import Footer from './components/footer/Footer.vue';
+</script>
 
-<template></template>
+<template>
+  <Header />
+  <Main />
+  <Footer />
+</template>
 
 <style scoped></style>
