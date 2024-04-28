32e693a678b829a9ccfd296b2320a5164a5c9958
RalfiSlask <nilssonnarki@gmail.com>
2024-04-28 14:00:28 +0200

    :sparkles: add LIAPeriods component

---
+++ src/components/main/LIAPeriods.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <p>LIA-period 1: 14 oktober 2024 - 20 december 2024</p>
+    <p>LIA-period 2: 3 februari 2025 - 23 maj 2025</p>
+    <p>
+      Utbildningens hemsida:
+      <a href="https://medieinstitutet.se/utbildningar/front-end-developer/" target="_blank">Front End Developer</a>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
