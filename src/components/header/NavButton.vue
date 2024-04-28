4cd3a3d28c8fef5ba6796fdc856ff218181a3d33
RalfiSlask <nilssonnarki@gmail.com>
2024-04-28 17:53:37 +0200

    :lipstick: change styling and overall responsivness

--- src/components/header/NavButton.vue
+++ src/components/header/NavButton.vue
@@ -1,5 +1,7 @@
 <template>
-  <a class="border-none h-[35px] w-[100px] cursor-pointer" :href="button.href">{{ button.text }}</a>
+  <li class="nav-button h-[35px] w-[100px]">
+    <a class="border-none cursor-pointer" :href="button.href">{{ button.text }}</a>
+  </li>
 </template>
 
 <script setup lang="ts">
