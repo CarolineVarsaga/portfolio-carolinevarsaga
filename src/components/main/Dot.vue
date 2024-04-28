ff752e089f8cc8a55059fe74b1a2a09f92fcac1d
RalfiSlask <nilssonnarki@gmail.com>
2024-04-28 17:50:52 +0200

    :sparkles: create Dot component with props for changing active state

---
+++ src/components/main/Dot.vue
@@ -0,0 +1,25 @@
+<template>
+  <span
+    @click="
+      () => {
+        changeState(props.number);
+      }
+    "
+    :class="{
+      'bg-backgroundThird': props.state,
+    }"
+    class="cursor-pointer h-[15px] w-[15px] my-0 mx-[2px] rounded-full inline-block text-center hover:bg-backgroundThird bg-backgroundDot transition-colors duration-500 ease"
+  ></span>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  state: boolean;
+  number: number;
+  changeState: (state: number) => void;
+}>();
+</script>
+
+<style scoped></style>
