caefa0a370e2f64ab6026f76100ba0386627ec1f
RalfiSlask <nilssonnarki@gmail.com>
2024-04-29 20:17:29 +0200

    :sparkles: add main component with all sections of the project

---
+++ src/components/main/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="flex flex-col gap-10">
+    <Landing />
+    <About />
+    <LIA />
+    <Projects />
+    <Contact />
+  </main>
+</template>
+
+<script setup lang="ts">
+import Landing from './Landing.vue';
+import About from './About.vue';
+import LIA from './LIA.vue';
+import Projects from './Projects.vue';
+import Contact from './Contact.vue';
+</script>
+
+<style scoped></style>
