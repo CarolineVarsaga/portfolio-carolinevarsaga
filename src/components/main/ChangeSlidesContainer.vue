721233131427f969520deccb7680d3e96e60d56f
RalfiSlask <nilssonnarki@gmail.com>
2024-04-29 20:16:37 +0200

    :sparkles: add change slides component that contains prev and next arrows

---
+++ src/components/main/ChangeSlidesContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a class="prev">
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#fff">
+      <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+    </svg>
+  </a>
+  <a class="next">
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#fff">
+      <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+    </svg>
+  </a>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
