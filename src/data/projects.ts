9570a91454f8aef6f13fb58b967d55ca3ecbbb6a
RalfiSlask <nilssonnarki@gmail.com>
2024-04-29 20:18:01 +0200

    :sparkles: add projects.ts file in data folder with an array of objects for the projects

---
+++ src/data/projects.ts
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    title: 'Sänka skepp, multiplayer, Socket io',
+    description: 'Gruppuppgift i API-kursen. Vi skulle bygga ett spel där man möter varandra i realtid.',
+    href: 'https://github.com/GridSock1/battleship-royal',
+    imgsrc: '/battleship.png',
+    alt: 'spel, sänka skepp',
+  },
+  {
+    title: 'Dokumenthanterinssystem',
+    description: 'API-uppgift med MySQL. Innerhåller WYSIWYG-editor.',
+    href: 'https://github.com/CarolineVarsaga/notes-api',
+    imgsrc: '/api-editor.png',
+    alt: 'Startsida där man ser senaste anteckningar man gjort',
+  },
+  {
+    title: 'Quiz med TypeScript',
+    description: 'Som gruppuppgift i JavaScript kursen fick vi i uppdrag att skapa ett quiz med TypeScript',
+    href: 'https://github.com/CarolineVarsaga/quiz',
+    imgsrc: '/quiz2.jpg',
+    alt: 'quiz med gummiankor',
+  },
+  {
+    title: 'Kommer snart...',
+    description: 'Uppdaterar här med mina senaste projekt. Spana gärna in min GitHub för mer.',
+    href: 'https://github.com/CarolineVarsaga',
+    imgsrc: '/desk_mobile.jpg',
+    alt: 'spel, sänka skepp',
+  },
+];
