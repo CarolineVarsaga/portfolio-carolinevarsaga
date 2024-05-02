8ae86cc00a26ececa012312eb7f21ea6be06cb01
RalfiSlask <nilssonnarki@gmail.com>
2024-05-02 05:51:36 +0200

    :sparkles: add shared medium heading component

---
+++ src/components/shared/MediumHeading.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3 class="medium-heading">{{ text }}</h3>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{ text: string }>();
+</script>
+
+<style scoped></style>
