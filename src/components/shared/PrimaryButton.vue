abf73875f29aac344b0308be9375390af7ccdd82
RalfiSlask <nilssonnarki@gmail.com>
2024-05-02 05:51:03 +0200

    :sparkles: add scroll function for smooth scrolling on click

--- src/components/shared/PrimaryButton.vue
+++ src/components/shared/PrimaryButton.vue
@@ -1,11 +1,23 @@
 <template>
-  <button class="primary-button">{{ text }}</button>
+  <a @click.prevent="scrollToAnchor" :href="props.href">
+    <button class="primary-button">{{ props.text }}</button>
+  </a>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue';
 
-const props = defineProps<{ text: string }>();
+const props = defineProps<{ text: string; href?: string }>();
+
+const scrollToAnchor = async () => {
+  if (props.href) {
+    const anchor = document.querySelector(props.href);
+    console.log(anchor);
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+};
 </script>
 
 <style scoped></style>
